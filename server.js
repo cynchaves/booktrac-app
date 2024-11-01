84a42d16ae8323729e907acb7bacc1e2339233a8
Cynthia Chaves <cynthia_chaves@aol.com>
2024-10-31 23:23:18 -0400

    Added css image and font styling across all views.

--- server.js
+++ server.js
@@ -18,9 +18,10 @@ const port = process.env.PORT ? process.env.PORT : '3000';
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 });
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
